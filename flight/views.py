9d952219d22984de4c66c820f44cd9d3cf68d30f
arnavtripathy <arnavtripathy98@gmail.com>
2024-11-10 16:50:56 +0000

    Added few more details in flight API

--- flight/views.py
+++ flight/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 from django.conf import settings
 from rest_framework.decorators import api_view
 from datetime import datetime, timedelta
-
+import time
 # Create your views here.
 
 # Fetch the API key from settings or a file
@@ -24,15 +24,39 @@ def fetch_possible_dates(request, search_param):
 
         # Filter to include only the next 10 dates from today
         today = datetime.now().date()
-        ten_days_from_today = today + timedelta(days=10)
-        
-        filtered_dates = [
-            date_str for date_str in data
-            if today <= datetime.strptime(date_str, "%Y-%m-%d").date() <= ten_days_from_today
-        ][:10]  # Limit to the first 10 days
-
-        return JsonResponse(filtered_dates, safe=False)
-
+        # Sort and filter dates after today, then select the first 10
+        filtered_dates = sorted(
+            [date_str for date_str in data if datetime.strptime(date_str, "%Y-%m-%d").date() >= today]
+        )[:10]
+        print(filtered_dates)
     except requests.exceptions.RequestException as e:
         return JsonResponse({"error": "Failed to fetch dates, flight does not exist or dates are not near enough"}, status=404)
+
+    flight_details =[]
+    print(type(filtered_dates))
+    for date in filtered_dates:
+        FLIGHT_DETAILS_URL= f"https://aerodatabox.p.sulu.sh/flights/Number/{search_param}/{date}?withAircraftImage=false&withLocation=false"
+        try:
+            response = requests.get(FLIGHT_DETAILS_URL, headers=headers)
+            response = response.json()
+
+            flight_number_detail = [{
+                "date": date,
+                "departure_airport": response[0]["departure"]["airport"]["shortName"],
+                "arrival_airport": response[0]["arrival"]["airport"]["shortName"],
+                "departure_local_time": response[0]["departure"]["scheduledTime"]["utc"],
+                "arrival__local_time": response[0]["arrival"]["scheduledTime"]["utc"],
+                "airline_name": response[0]["airline"]["name"]
+            }]
+            flight_details = flight_details + flight_number_detail
+        except requests.exceptions.RequestException as e:
+            flight_number_detail = [{
+                "date": date,
+                "error": "Coudn't fetch details of this flight for this date"
+            }]
+            flight_details = flight_details + flight_number_detail
+        time.sleep(1) #To avoid being rate limited
+
+    return JsonResponse(flight_details, safe=False)
+
 
