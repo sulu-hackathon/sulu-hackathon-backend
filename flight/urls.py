9e98ea4c8efa4d19b6db1d3323c71a272dc64b61
arnavtripathy <arnavtripathy98@gmail.com>
2024-11-11 11:26:49 +0000

    Added autocomplete response for flight numbers

--- flight/urls.py
+++ flight/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('fetch_flight_details/<str:search_param>/', views.fetch_flight_details, name='fetch_flight_details'),
-    path('add_flight_details/', views.add_flight_details, name='add_flight_details')
+    path('add_flight_details/', views.add_flight_details, name='add_flight_details'),
+    path('autocomplete_flight_number/<str:flight_number>/', views.autocomplete_flight_number, name='autocomplete_flight_number')
 ]
